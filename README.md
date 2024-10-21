4267d031a67996e1fe0d50d71e9613df0663d2ac
uerbzr <nigel.sibbert@gmail.com>
2024-10-21 07:26:31 +0100

    updated

--- README.md
+++ README.md
@@ -17,18 +17,16 @@
 
 For this exercise you should get a a full stack web application working in Azure. You can create something simple and new or you may use an existing application, e.g. the cinema challenge and build a simple front end using technology you choose. You should build using the following approach:
 
-- Back end API using C# and the `dotnet new webapi` template with at least 1 endpoint
-- Front end using either `React` or `Angular` which makes at least one call to the API endpoint.  You frontend repository should be pushed [here](https://github.com/boolean-uk/csharp-cloud-azure-day-1-frontend)
-- Database using PostgresSQL hosted in Azure
+- Use the provided back end ToDo API 
+- Use the provided React Front end and host in an Azure Static Website
 
 ## Extension Activity
 
-- Any extended functionality you give your API will be awarded an Extension.
+- Swap out the InMemory database for a cloud hosted one (db credentials will be provided)
 
 ## Notes
 
-We will be only using free tiers to create Azure items.
-
+- We will be only using free tiers to create Azure items.
 - Use the [Microsoft Azurite Storage Emulator](https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azurite?tabs=visual-studio%2Cblob-storage)
 - Use the [Microsoft Azure Storage Explorer](https://azure.microsoft.com/en-us/products/storage/storage-explorer/)
-- FYI this is a legacy product: [Microsoft Azure Storage Emulator](https://learn.microsoft.com/en-us/azure/storage/common/storage-use-emulator)
+- FYI this is a legacy product (so ignore!): [Microsoft Azure Storage Emulator](https://learn.microsoft.com/en-us/azure/storage/common/storage-use-emulator)
--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -1,11 +1,32 @@
+using exercise.wwwapi.EndPoints;
+using exercise.wwwapi.Models;
+using exercise.wwwapi.Repository;
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+// Add CORS service with policy that allows any origin
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDatabaseRepository<Todo>, DatabaseRepository<Todo>>();
 
 var app = builder.Build();
+
+// Use the CORS policy
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,31 +35,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.ConfigureTodoAPI();
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
